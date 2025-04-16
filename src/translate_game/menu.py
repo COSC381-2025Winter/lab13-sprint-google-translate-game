7ee45a659b6f28b474b2fc78ab7cbf40b4649119
Patrick Martus <115840180+Pkmartus@users.noreply.github.com>
2025-04-16 15:34:36 +0000

    removed unnessesary while loop from mainMenu

--- src/translate_game/menu.py
+++ src/translate_game/menu.py
@@ -1,19 +1,18 @@
 def mainMenu():
-    while(True):
-        print("Welcome to the Translation Game")
-        print("Language options: ")
-        print("1. Korean")
-        print("2. Spanish")
-        print("3. Manderin")
-        print("q: quit")
-        choice = input("Enter a number to select a language: ")
-        if choice == 'q':
-            return 'q'
-        choice = int (choice)
-        if choice == 1:
-            language = "ko"
-        if choice == 2:
-            language = "es"
-        if choice == 3:
-            language = "zh"
-        return language
+    print("Welcome to the Translation Game")
+    print("Language options: ")
+    print("1. Korean")
+    print("2. Spanish")
+    print("3. Manderin")
+    print("q: quit")
+    choice = input("Enter a number to select a language: ")
+    if choice == 'q':
+        return 'q'
+    choice = int (choice)
+    if choice == 1:
+        language = "ko"
+    if choice == 2:
+        language = "es"
+    if choice == 3:
+        language = "zh"
+    return language
