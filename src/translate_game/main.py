e072e6b0993063f3bf3373434a67249054e21dd1
Adam Snowgold <196842243+AdamSnow12@users.noreply.github.com>
2025-04-17 14:39:07 -0400

    Added function calls to main.py - Modified selectingQuizQuestions.py imports

--- src/translate_game/main.py
+++ src/translate_game/main.py
@@ -1,6 +1,11 @@
 from translation import translate_text
+from menu import mainMenu
+from selectingQuizQuestions import quiz
+from score_board import evaluate_quiz
 
 def main():
-    translate_text("Hello, how are you?", "en", "fr")
+    language = mainMenu()
+    userAnswers, correctAnswers, language = quiz(language)
+    evaluate_quiz(userAnswers, correctAnswers, language)
 
 main()
